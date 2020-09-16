732be96b96b50cfe781717ff4b9dad24af57c904
Francois Swanepoel <francoisjeanswanepoel@gmail.com>
2020-09-16 07:06:29 +0000

    Pagination

--- src/api/model/Contacts.php
+++ src/api/model/Contacts.php
@@ -10,8 +10,17 @@ class Contacts extends Model
     protected int $filter = FILTER_SANITIZE_NUMBER_INT;
 
     function process() {
-        $page = array_key_exists(0, $this->params) ? intval($this->params[0]) : 0;
-        return json_encode($this->dbo->getContacts($_ENV['OFFSET'], Offset::getOffset($_ENV['OFFSET'], $page, $this->dbo->getContactCount())), JSON_UNESCAPED_UNICODE);
+	    $page = array_key_exists(0, $this->params) ? intval($this->params[0]) : 0;
+	    $rec_count = $this->dbo->getContactCount();
+	    $offset = Offset::getOffset($_ENV['OFFSET'], $page, $this->dbo->getContactCount());
+	    $data = [
+		    'pagination' => [
+		    	'total_records' => $rec_count,
+			'offset' => $offset
+		    ],
+		'data' => $this->dbo->getContacts($_ENV['OFFSET'], $offset)
+	    ];
+	    return json_encode($data, JSON_UNESCAPED_UNICODE);
     }
 
 }
